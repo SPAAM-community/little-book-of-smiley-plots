8446461f0634e4c5d0830ec110be08dbacc9300a
James Fellows Yates <jfy133@gmail.com>
2023-06-24 14:55:19 +0200

    Add Egbert and contributors page with some additional clean up

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ To add a new plot:
 1. add a new `.qmd` markdown file (make a copy of `template.qmd` if you wish)
 2. add the name of the file to the corresponding 'part' in `_quarto.yml`.
 3. raw deamination plot data file(s) (e.g. from mapDamage or DamageProfiler) for the corresponding plot should be added to the `assets/data/` folder in a subfolder named the same as the `.qmd` file.
-4. caricature image should go in `assets/images` also with the same name as the `.qmd`.
+4. caricature image should go in `assets/images/` in a folder also with the same name as the `.qmd`, but with the file name the 'name' of the caricature.
 5. bibtex citations should go in `references.bib`
--- assets/src/R/damage-profiler.r
+++ assets/src/R/damage-profiler.r
@@ -34,18 +34,34 @@ pivot_longer_freqmisincorporat <- function(x, reverse, type_colours) {
 ##############
 
 plot_longer_freqmisincorporat <- function(x, reverse, type_colours) {
-    result <- ggplot2::ggplot(x, aes(x = Position, y = Frequency, colour = `Mutation Type`)) +
+    result <- ggplot2::ggplot(
+        x,
+        aes(
+            x = Position,
+            y = Frequency,
+            colour = `Mutation Type`
+        )
+    ) +
         ggplot2::geom_line() +
-        ggplot2::scale_colour_manual(values = type_colours, guide = ggplot2::guide_legend(reverse = TRUE)) +
+        ggplot2::scale_colour_manual(
+            values = type_colours,
+            guide = ggplot2::guide_legend(reverse = TRUE)
+        ) +
         ggplot2::facet_wrap(~end, ncol = 2) +
         ggplot2::theme_classic() +
-        ggplot2::theme(legend.position = "none") +
+        ggplot2::theme(
+            legend.position = "none",
+            panel.background = element_rect(fill = "transparent")
+        ) +
         ggplot2::coord_cartesian(ylim = c(0, 0.3))
 
     if (reverse) {
         result <- result +
             ggplot2::scale_x_continuous(breaks = seq(0, -14, -1)) +
-            ggplot2::scale_y_continuous(breaks = seq(0, 0.30, 0.05), position = "right")
+            ggplot2::scale_y_continuous(
+                breaks = seq(0, 0.30, 0.05),
+                position = "right"
+            )
     } else {
         result <- result +
             ggplot2::scale_x_continuous(breaks = seq(0, 14, 1)) +
